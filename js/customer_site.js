0d163e827169c6d127e84e617212adb7e80d5686
gqw <givenge@hotmail.com>
2025-08-14 17:25:17 +0800

    fix

--- js/customer_site.js
+++ js/customer_site.js
@@ -7,7 +7,7 @@ const CUSTOMER_SITES = {
 
 // 调用全局方法合并
 if (window.extendAPISites) {
-    window.extendAPISites(CUSTOMER_SITES);
+    window.extendAPISites(API_SITES);
 } else {
     console.error("错误：请先加载 config.js！");
 }
